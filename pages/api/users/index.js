eaaf9f01212385114566fd1e07b5898c41fbc1be
Abel Laime <abel.laime@editorialaces.com>
2023-01-18 15:18:21 -0300

    oks

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -2,7 +2,7 @@ import connectMongo from '../../../database/conn'
 import { getUsers, postUser, putUser, deleteUser } from '../../../database/controller';
 
 export default async function handler(req, res) {
-    connectMongo().catch(() => res.status(405).json({ error: "Error in the Connection"}))
+    connectMongo().catch(() => res.status(405).json({ error: "Error in the Connection!!"}))
 
     // type of request
     const { method } = req
